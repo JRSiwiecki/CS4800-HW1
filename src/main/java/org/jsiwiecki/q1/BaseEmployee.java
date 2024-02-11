b7f3b17e0b87782ed318262047d4704c36a442b9
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-10 20:06:05 -0800

    Add baseemployee class

---
+++ src/main/java/org/jsiwiecki/q1/BaseEmployee.java
@@ -0,0 +1,22 @@
+package org.jsiwiecki.q1;
+
+public class BaseEmployee extends Employee
+{
+    private int baseSalary;
+
+    public BaseEmployee(String firstName, String lastName, String socialSecurityNumber, int baseSalary)
+    {
+        super(firstName, lastName, socialSecurityNumber);
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary()
+    {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(int baseSalary)
+    {
+        this.baseSalary = baseSalary;
+    }
+}
