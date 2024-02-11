19a96dce13d5aeb472c3839963c9ad78e0eae283
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-10 23:14:13 -0800

    Update q4 to use composition (but still stupidly)

--- src/main/java/org/jsiwiecki/q4/Main.java
+++ src/main/java/org/jsiwiecki/q4/Main.java
@@ -5,55 +5,37 @@ public class Main
     public static void main(String[] args)
     {
         Folder phpDemo1 = new Folder("php_demo1");
-        Folder sourceFiles = new Folder("Source Files");
-        phpDemo1.addFolder(sourceFiles);
+        phpDemo1.addFolder(new Folder("Source Files"));
 
-        Folder phalcon = new Folder(".phalcon");
-        Folder app = new Folder("app");
-        sourceFiles.addFolder(phalcon);
-        sourceFiles.addFolder(app);
+        phpDemo1.getSubFolders().get(0).addFolder(new Folder(".phalcon"));
+        phpDemo1.getSubFolders().get(0).addFolder(new Folder("app"));
 
-        Folder config = new Folder("config");
-        Folder controllers = new Folder("controllers");
-        Folder library = new Folder("library");
-        Folder migrations = new Folder("migrations");
-        Folder models = new Folder("models");
-        Folder views = new Folder("views");
+        phpDemo1.getSubFolders().get(0).getSubFolders().get(1).addFolder(new Folder("config"));
+        phpDemo1.getSubFolders().get(0).getSubFolders().get(1).addFolder(new Folder("controllers"));
+        phpDemo1.getSubFolders().get(0).getSubFolders().get(1).addFolder(new Folder("library"));
+        phpDemo1.getSubFolders().get(0).getSubFolders().get(1).addFolder(new Folder("migrations"));
+        phpDemo1.getSubFolders().get(0).getSubFolders().get(1).addFolder(new Folder("models"));
+        phpDemo1.getSubFolders().get(0).getSubFolders().get(1).addFolder(new Folder("views"));
 
-        app.addFolder(config);
-        app.addFolder(controllers);
-        app.addFolder(library);
-        app.addFolder(migrations);
-        app.addFolder(models);
-        app.addFolder(views);
+        phpDemo1.getSubFolders().get(0).addFolder(new Folder("cache"));
+        phpDemo1.getSubFolders().get(0).addFolder(new Folder("public"));
 
-        Folder cache = new Folder("cache");
-        Folder publicFolder = new Folder("public");
-        sourceFiles.addFolder(cache);
-        sourceFiles.addFolder(publicFolder);
+        phpDemo1.getSubFolders().get(0).addFile(new File(".htaccess"));
+        phpDemo1.getSubFolders().get(0).addFile(new File(".htrouter.php"));
+        phpDemo1.getSubFolders().get(0).addFile(new File("index.html"));
 
-        File htaccess = new File(".htaccess");
-        File htrouter = new File(".htrouter.php");
-        File index = new File("index.html");
-        sourceFiles.addFile(htaccess);
-        sourceFiles.addFile(htrouter);
-        sourceFiles.addFile(index);
-
-        Folder path = new Folder("Include Path");
-        Folder remote = new Folder("Remote Path");
-
-        phpDemo1.addFolder(path);
-        phpDemo1.addFolder(remote);
+        phpDemo1.addFolder(new Folder("Include Path"));
+        phpDemo1.addFolder(new Folder("Remote Path"));
 
         System.out.println("--- INITIAL PRINT ---");
         phpDemo1.print();
 
         System.out.println("--- AFTER DELETING APP ---");
-        phpDemo1.deleteFolder(app);
+        phpDemo1.deleteFolder(phpDemo1.getSubFolders().get(0).getSubFolders().get(1));
         phpDemo1.print();
 
         System.out.println("--- AFTER DELETING PUBLIC ---");
-        phpDemo1.deleteFolder(publicFolder);
+        phpDemo1.deleteFolder(phpDemo1.getSubFolders().get(0).getSubFolders().get(2));
         phpDemo1.print();
     }
 }
