25dce79e47e2eb92eec4df2872b46bbb8cbc00fe
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-10 20:03:39 -0800

    Add hourly employee class

---
+++ src/main/java/org/jsiwiecki/q1/HourlyEmployee.java
@@ -0,0 +1,34 @@
+package org.jsiwiecki.q1;
+
+public class HourlyEmployee extends Employee
+{
+    private int hourlyWage;
+    private int hoursWorked;
+
+    public HourlyEmployee(String firstName, String lastName, String socialSecurityNumber, int hourlyWage, int hoursWorked)
+    {
+        super(firstName, lastName, socialSecurityNumber);
+        this.hourlyWage = hourlyWage;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getHourlyWage()
+    {
+        return hourlyWage;
+    }
+
+    public void setHourlyWage(int hourlyWage)
+    {
+        this.hourlyWage = hourlyWage;
+    }
+
+    public int getHoursWorked()
+    {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked)
+    {
+        this.hoursWorked = hoursWorked;
+    }
+}
