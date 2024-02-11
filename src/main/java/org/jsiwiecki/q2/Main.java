f2801fcd1b6ada57f6b23cc85385cb549cd8828b
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-10 20:33:14 -0800

    Add main testing for shipsg

--- src/main/java/org/jsiwiecki/q2/Main.java
+++ src/main/java/org/jsiwiecki/q2/Main.java
@@ -4,6 +4,15 @@ public class Main
 {
     public static void main(String[] args)
     {
+        Ship[] ships = new Ship[3];
 
+        ships[0] = new Ship("USS Enterprise", "2266");
+        ships[1] = new CruiseShip("Millennium Falcon", "1750", 50);
+        ships[2] = new CargoShip("Battlestar Galactica", "1969", 100);
+
+        for (Ship ship: ships)
+        {
+            ship.print();
+        }
     }
 }
