15912ba222eec7383e5d30286cdb84dc031716cd
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-10 20:02:33 -0800

    Add SalariedEmployee

---
+++ src/main/java/org/jsiwiecki/q1/SalariedEmployee.java
@@ -0,0 +1,22 @@
+package org.jsiwiecki.q1;
+
+public class SalariedEmployee extends Employee
+{
+    private int weeklySalary;
+
+    public SalariedEmployee(String firstName, String lastName, String socialSecurityNumber, int weeklySalary)
+    {
+        super(firstName, lastName, socialSecurityNumber);
+        this.weeklySalary = weeklySalary;
+    }
+
+    public int getWeeklySalary()
+    {
+        return weeklySalary;
+    }
+
+    public void setWeeklySalary(int weeklySalary)
+    {
+        this.weeklySalary = weeklySalary;
+    }
+}
