191f62c48fc74aceb7a388bbef5989cd21c6459c
JRSiwiecki <JRSiwiecki@gmail.com>
2024-02-13 17:42:14 -0800

    Cleanup q1 print

--- src/main/java/org/jsiwiecki/q1/Main.java
+++ src/main/java/org/jsiwiecki/q1/Main.java
@@ -4,20 +4,19 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Employee joeJones = new SalariedEmployee("Joe", "Jones", "111-11-1111", 2500);
-        Employee stephanieSmith = new HourlyEmployee("Stephanie", "Smith", "222-22-2222", 25, 32);
-        Employee maryQuinn = new HourlyEmployee("Mary", "Quinn", "333-33-3333", 19, 47);
-        Employee nicoleDior = new CommissionEmployee("Nicole", "Dior", "444-44-4444", 0.15, 50_000);
-        Employee renwaChanel = new SalariedEmployee("Renwa", "Chanel", "555-55-5555", 1700);
-        Employee mikeDavenport = new BaseEmployee("Mike", "Davenport", "666-66-6666", 95000);
-        Employee mahnazVaziri = new CommissionEmployee("Mahnaz", "Vaziri", "777-77-7777", 0.22, 40_000);
+        Employee[] employees = new Employee[7];
 
-        System.out.println(joeJones);
-        System.out.println(stephanieSmith);
-        System.out.println(maryQuinn);
-        System.out.println(nicoleDior);
-        System.out.println(renwaChanel);
-        System.out.println(mikeDavenport);
-        System.out.println(mahnazVaziri);
+        employees[0] = new SalariedEmployee("Joe", "Jones", "111-11-1111", 2500);
+        employees[1] = new HourlyEmployee("Stephanie", "Smith", "222-22-2222", 25, 32);
+        employees[2] = new HourlyEmployee("Mary", "Quinn", "333-33-3333", 19, 47);
+        employees[3] = new CommissionEmployee("Nicole", "Dior", "444-44-4444", 0.15, 50_000);
+        employees[4] = new SalariedEmployee("Renwa", "Chanel", "555-55-5555", 1700);
+        employees[5] = new BaseEmployee("Mike", "Davenport", "666-66-6666", 95000);
+        employees[6] = new CommissionEmployee("Mahnaz", "Vaziri", "777-77-7777", 0.22, 40_000);
+
+        for (Employee employee: employees)
+        {
+            System.out.println(employee);
+        }
     }
 }
